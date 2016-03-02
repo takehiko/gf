335c0f7bdd01c91e504a4cbf4312e73024548672
takehiko <takehiko@sys.wakayama-u.ac.jp>
2016-03-02 15:55:59 +0900

    fit to root.rb

--- lib/gf/yagura.rb
+++ lib/gf/yagura.rb
@@ -17,15 +17,20 @@ module GFLoad
 
     def build_yagura5
       # やぐら(5人技)を構成
-      p = GFLoad::Person.new(:name => "1.1"); add_person(p)
-      p = GFLoad::Person.new(:name => "1.2"); add_person(p)
-      p = GFLoad::Person.new(:name => "2.1"); add_person(p)
-      p.put_load(@mem["1.1"], 0.3)
-      p = GFLoad::Person.new(:name => "2.2"); add_person(p)
-      p.put_load(@mem["1.2"], 0.3)
-      p = GFLoad::Person.new(:name => "3.1"); add_person(p)
-      p.put_load(@mem["2.1"], 0.5)
-      p.put_load(@mem["2.2"], 0.5)
+      p11 = GFLoad::Person.new(:name => "1.1"); add_person(p11)
+      p12 = GFLoad::Person.new(:name => "1.2"); add_person(p12)
+      p21 = GFLoad::Person.new(:name => "2.1"); add_person(p21)
+      p21.put_load(p11, 0.3)
+      p22 = GFLoad::Person.new(:name => "2.2"); add_person(p22)
+      p22.put_load(p12, 0.3)
+      p31 = GFLoad::Person.new(:name => "3.1"); add_person(p31)
+      p31.put_load(p21, 0.5)
+      p31.put_load(p22, 0.5)
     end
   end
 end
+
+
+
+
+
